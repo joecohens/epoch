dad840b9e4178a1394f06dc582c486f3b5a7d312
Joe Cohen <joecohens@users.noreply.github.com>
2024-04-09 23:26:56 -0600

    enable vercel analytics and speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import "./globals.css"
 
 import { Inter as FontSans } from "next/font/google"
+
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import { ThemeProvider } from "@/components/theme-provider"
 
 import { cn } from "@/lib/utils"
@@ -36,6 +40,8 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
