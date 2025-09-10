b87df1afef02d9d98c40ebb79fabca3a251eb5dd
Sabari S <incrediblesabari02@gmail.com>
2025-09-10 08:30:21 +0530

    fix: fix padding in PopoverContent

--- components/timezone-selector.tsx
+++ components/timezone-selector.tsx
@@ -50,7 +50,7 @@ export default function TimezoneSelector({ timezones, currentTz, setCurrentTz }:
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-full p-5">
+      <PopoverContent className="w-full">
         <Command
           filter={(value, search) => {
             if (value.toLowerCase().includes(search.toLowerCase())) {
